ea67526c06e8ee81e9751cd64ae21d252df677b6
Lucas Jeong <jcy9503@naver.com>
2023-07-17 18:47:33 +0900

    Screen control feature added.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,20 @@
-using System;
+/*
+ * GameManager.cs
+ * --------------
+ * Made by Lucas Jeong
+ * Contains main game logic and singleton instance.
+ * Also contains screen control method.
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UIElements;
 
-public class GameManager : MonoBehaviour, IBeginDragHandler, IDragHandler
+public class GameManager : MonoBehaviour
 {
-	private static readonly object locker = new();
-
-	private static bool shuttingDown;
-
-	private static GameManager instance;
-
+	private static readonly object      locker = new();
+	private static          bool        shuttingDown;
+	private static          GameManager instance;
 	public static GameManager Instance
 	{
 		get
@@ -29,6 +31,7 @@ public class GameManager : MonoBehaviour, IBeginDragHandler, IDragHandler
 				if (instance != null) return instance;
 
 				instance = FindObjectOfType<GameManager>();
+
 				if (instance == null)
 				{
 					instance = new GameObject("GameManager").AddComponent<GameManager>();
@@ -51,58 +54,24 @@ public class GameManager : MonoBehaviour, IBeginDragHandler, IDragHandler
 		shuttingDown = true;
 	}
 
-	private static bool gameOver;
-
-	private Camera     mainCamera;
-	private Transform  cameraRotator;
-	private Quaternion cameraRotation;
-	private Vector2    _angleUpdate;
-	private Vector2 AngleUpdate
-	{
-		get => _angleUpdate;
-		set
-		{
-			_angleUpdate.y = value.y;
-			if (value.x > initialCameraRotationX + cameraRotationConstraintX)
-				_angleUpdate.x = initialCameraRotationX + cameraRotationConstraintX;
-			else if (value.x < initialCameraRotationX - cameraRotationConstraintX)
-				_angleUpdate.x = initialCameraRotationX - cameraRotationConstraintX;
-		}
-	}
-	private                  Vector2 angleTemp;
-	private                  Vector2 clickPos;
-	[SerializeField] private float   initialCameraRotationX    = 15f;
-	[SerializeField] private float   cameraRotationConstraintX = 55f;
-	[SerializeField] private float   cameraSpeed               = 10f;
-
-	[SerializeField] private int gridSizeX = 10;
-	[SerializeField] private int gridSizeY = 22;
-	[SerializeField] private int gridSizeZ = 10;
-
-	public static  GameGrid   Grid;
-	private static BlockQueue blockQueue;
-
-	[SerializeField] private float blockSize = 1.0f;
-	[SerializeField] private float interval  = 1.0f;
-
-	private List<Coroutine> func;
-
 	private void Awake()
 	{
 		gameOver = false;
 
 		mainCamera                     = Camera.main;
 		mainCamera!.transform.rotation = Quaternion.Euler(initialCameraRotationX, 0f, 0f);
-		cameraRotator                  = GameObject.Find("Camera Rotator").GetComponent<Transform>();
-		cameraRotation                 = cameraRotator.rotation;
-		AngleUpdate                    = new Vector2(cameraRotation.eulerAngles.x, cameraRotation.eulerAngles.y);
+		rotatorTr                      = GameObject.Find("Rotator").GetComponent<Transform>();
+		rotatorRt                      = rotatorTr.rotation;
+		mementoRotation                = Quaternion.identity;
+		Dir                            = false;
+		checkDir                       = false;
 
 		Grid       = new GameGrid(gridSizeX, gridSizeY, gridSizeZ, blockSize);
 		blockQueue = new BlockQueue();
 
 		func = new List<Coroutine>
 		{
-			StartCoroutine(BlockDown())
+			StartCoroutine(BlockDown()),
 		};
 	}
 
@@ -112,14 +81,97 @@ public class GameManager : MonoBehaviour, IBeginDragHandler, IDragHandler
 		{
 			GameOver();
 		}
+		else
+		{
+#if UNITY_EDITOR || UNITY_STANDALONE_WIN
+			if (Input.GetMouseButtonDown(0))
+			{
+				mementoRotation = rotatorTr.rotation;
+				clickPos        = Vector2.zero;
+			}
+
+			if (Input.GetMouseButton(0))
+			{
+				Vector2 angle = new(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"));
+
+				clickPos += angle;
+				Dir      =  Mathf.Abs(clickPos.x) > Mathf.Abs(clickPos.y);
+
+				if (checkDir)
+				{
+					rotatorTr.rotation = mementoRotation;
+					checkDir           = false;
+					clickPos           = Vector2.zero;
+				}
+
+				Quaternion target = rotatorTr.rotation;
+
+				if (Dir)
+				{
+					target *= Quaternion.AngleAxis(angle.x, Vector3.right);
+				}
+				else
+				{
+					target *= Quaternion.AngleAxis(angle.y, Vector3.up);
+				}
+
+				float angleX = target.eulerAngles.x;
+				angleX = angleX > 180 ? angleX - 360 : angleX;
+				target.eulerAngles = new Vector3(Mathf.Clamp(angleX, -cameraRotationConstraintX,
+				                                             cameraRotationConstraintX),
+				                                 target.eulerAngles.y, 0f);
+				rotatorTr.rotation = Quaternion.RotateTowards(rotatorTr.rotation, target,
+				                                              cameraSpeed * Time.deltaTime);
+			}
+
+			if (Input.GetMouseButtonUp(0))
+			{
+				mementoRotation = rotatorTr.rotation;
+			}
+#endif
+		}
 	}
+
+	private static bool       gameOver;
+	private        Camera     mainCamera;
+	private        Transform  rotatorTr;
+	private        Quaternion rotatorRt;
+	private        Quaternion mementoRotation;
+	private        bool       checkDir;
+	private        bool       _dir;
+	private bool Dir
+	{
+		get => _dir;
+		set
+		{
+			if (value != _dir)
+				checkDir = true;
+			_dir = value;
+		}
+	}
+	private                  Vector2         clickPos;
+	[SerializeField] private float           initialCameraRotationX    = 15f;
+	[SerializeField] private float           cameraRotationConstraintX = 55f;
+	[SerializeField] private float           cameraSpeed               = 2000f;
+	[SerializeField] private int             gridSizeX                 = 10;
+	[SerializeField] private int             gridSizeY                 = 22;
+	[SerializeField] private int             gridSizeZ                 = 10;
+	public static            GameGrid        Grid;
+	private static           BlockQueue      blockQueue;
+	[SerializeField] private float           blockSize = 1.0f;
+	[SerializeField] private float           interval  = 1.0f;
+	private                  List<Coroutine> func;
 
 	private IEnumerator BlockDown()
 	{
 		while (true)
 		{
 			if (!Grid.IsPlaneEmpty(0))
+			{
 				gameOver = true;
+
+				break;
+			}
 
 			blockQueue.Current.Move(-Vector3.down);
 
@@ -133,31 +185,5 @@ public class GameManager : MonoBehaviour, IBeginDragHandler, IDragHandler
 		{
 			StopCoroutine(coroutine);
 		}
-	}
-
-	public void OnBeginDrag(PointerEventData eventData)
-	{
-		BeginDrag(eventData.position);
-	}
-
-	public void OnDrag(PointerEventData eventData)
-	{
-		OnDrag(eventData.position);
-	}
-
-	private void BeginDrag(Vector2 pos)
-	{
-		clickPos  = pos;
-		angleTemp = AngleUpdate;
-		Debug.Log(pos.ToString());
-	}
-
-	private void OnDrag(Vector2 pos)
-	{
-		AngleUpdate = new Vector2(angleTemp.x + (pos.x - clickPos.x) * cameraSpeed / Screen.width,
-		                          angleTemp.y - (pos.y - clickPos.y) * cameraSpeed / Screen.height);
-		Debug.Log(AngleUpdate.ToString());
-
-		cameraRotation = Quaternion.Euler(AngleUpdate);
 	}
 }
